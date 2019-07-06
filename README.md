538170543aadfb04c24d3c3adb006a3d318e087f
jonathanadhitama <jonathanadhitama@users.noreply.github.com>
2019-07-06 23:21:35 +1000

    Middleware Server functionality

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Environment Variable:
+
+-   GOOGLE_MAP_API_KEY: GOOGLE MAPS API KEY
+
 # Starting the application:
 
 -   \$ DEBUG=hummingbird-middleware:\* npm start
+
+Due to this [issue](https://github.com/googlemaps/google-maps-services-js/issues/59#issuecomment-399626833), we need a middleware server to call Google Maps API.
--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const GOOGLE_MAPS_PLACES_URL =
+    "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+
+app.get("/places", (req, res) => {
+    if (Boolean(req.query) && Boolean(req.query.input)) {
+        axios
+            .get(GOOGLE_MAPS_PLACES_URL, {
+                params: {
+                    input: req.query.input,
+                    key: process.env.GOOGLE_MAP_API_KEY
+                }
+            })
+            .then(({ data, status }) => {
+                res.set("Content-Type", "application/json");
+                res.status(status);
+                res.send(data);
+            });
+    } else {
+        res.status(422);
+        res.send(null);
+    }
+});
 
 module.exports = app;
